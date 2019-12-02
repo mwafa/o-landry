f72d21e064118050f9ceea39efc43a603a599cd0
Muhammad Wafa <wafax.4@gmail.com>
2019-12-02 16:40:52 +0700

    cors allow all

--- core/application/controllers/Api.php
+++ core/application/controllers/Api.php
@@ -3,6 +3,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Api extends CI_Controller {
 
+    
+    public function __construct()
+    {
+        parent::__construct();
+        header("Access-Control-Allow-Origin: *");
+    }
+    
+
     public function cari()
     {
         $q = $this->input->get('q');
